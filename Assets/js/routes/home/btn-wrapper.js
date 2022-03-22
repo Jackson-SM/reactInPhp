c26d7a2214bd0a302bb0c2865df65012ffcd8171
Jackson <jacksonmagalhaes@hotmail.com>
2022-03-21 22:39:22 -0300

    Opcoes de botao criada na secao inicial, e apresentacao da section trocada

--- Assets/js/routes/home/btn-wrapper.js
+++ Assets/js/routes/home/btn-wrapper.js
@@ -1,6 +1,6 @@
 const go = document.querySelector('.btn-wrapper-go');
 const back = document.querySelector('.btn-wrapper-back');
-const items_container = document.querySelector('.initial_section');
+const items_container = document.querySelector('.wrapper_initial');
 
 go.addEventListener('click', event => {
   items_container.scrollBy(300,0);
